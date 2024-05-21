a49bdf09ef05d0bfcc327a8c233ec600986af76b
Daria Metelitsa <dariametelitsa@gmail.com>
2024-05-21 22:13:22 +0300

    hw5

--- src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
+++ src/s2-homeworks/hw04/common/c3-SuperCheckbox/SuperCheckbox.tsx
@@ -28,9 +28,8 @@ const SuperCheckbox: React.FC<SuperCheckboxPropsType> = (
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // задачка на написание онченджа
-        if(onChangeChecked) {
-            onChangeChecked(e.currentTarget.checked);
-        }
+        onChange?.(e)
+        onChangeChecked?.(e.currentTarget.checked);
 
     }
 
